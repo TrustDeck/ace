7282bda8cd4a00cc756306a1ebf9ec12284e5902
Eric Wündisch <eric.wuendisch@charite.de>
2025-07-17 09:51:53 +0200

    add isSuperDomain to createDomainGroupsAndRolesAndJoin

--- src/main/java/org/trustdeck/service/DomainOIDCService.java
+++ src/main/java/org/trustdeck/service/DomainOIDCService.java
@@ -96,9 +96,10 @@ public class DomainOIDCService {
      *
      * @param domainName the name of the domain to be created
      * @param userId the unique identifier of the user to be added to the groups
+     * @param isSuperDomain indicates if the domain is a superdomain
      * @throws NullPointerException if any group or role creation fails, or the user cannot be added to the groups
      */
-    public void createDomainGroupsAndRolesAndJoin(String domainName, String userId) {
+    public void createDomainGroupsAndRolesAndJoin(String domainName, String userId, boolean isSuperDomain) {
         // Create a new role for the specified domain name
         RoleRepresentation roleRepresentation = oidcService.createClientRole(domainName);
 
@@ -112,19 +113,30 @@ public class DomainOIDCService {
 
         // Iterate over each operation group and create a subgroup for the domain
         for (String operationGroupId : oidcService.getOperationGroupIDs()) {
-            GroupRepresentation groupRepresentation = oidcService.createSubGroup(operationGroupId, domainName);
 
-            // Ensure the subgroup was successfully created
-            if (groupRepresentation == null) {
-            	log.debug("Group for domain \"" + domainName + "\" could not be created.");
-                throw new DomainOIDCException(domainName);
+            boolean canCreate = true;
+            //make sure root operation are not related to non root domains
+            if(!isSuperDomain && oidcService.getRootOperationGroupIDs().contains(operationGroupId)){
+              canCreate = false;
             }
 
-            // Assign the new role to the created subgroup
-            oidcService.assignRoleToGroup(groupRepresentation.getId(), roleRepresentation);
+            if(canCreate){
+              // Create a subgroup for the domain name under the current operation group
+              GroupRepresentation groupRepresentation = oidcService.createSubGroup(operationGroupId, domainName);
 
-            // Add the newly created group's ID to the list
-            domainGroupIds.add(groupRepresentation.getId());
+              // Ensure the subgroup was successfully created
+              if (groupRepresentation == null) {
+                log.debug("Group for domain \"" + domainName + "\" could not be created.");
+                throw new DomainOIDCException(domainName);
+              }
+
+              // Assign the new role to the created subgroup
+              oidcService.assignRoleToGroup(groupRepresentation.getId(), roleRepresentation);
+
+              // Add the newly created group's ID to the list
+              domainGroupIds.add(groupRepresentation.getId());
+            }
+
         }
 
         // Ensure that the user is added to all operation-groups
@@ -255,11 +267,12 @@ public class DomainOIDCService {
      *
      * @param oldDomainName the name of the domain to be updated
      * @param newDomainName the new name to be assigned to the domain-related roles and groups
+     * @param isSuperDomain indicates if the domain is a superdomain
      * @throws NullPointerException if the new role cannot be created, or the old role cannot be deleted, or a group name update fails
      */
-    public void updateDomainGroups(String oldDomainName, String newDomainName, String userId) {
+    public void updateDomainGroups(String oldDomainName, String newDomainName, String userId, boolean isSuperDomain) {
         this.leaveAndDeleteDomainGroupsAndRoles(oldDomainName);
-        this.createDomainGroupsAndRolesAndJoin(newDomainName, userId);
+        this.createDomainGroupsAndRolesAndJoin(newDomainName, userId, isSuperDomain);
     }
     
     /**
