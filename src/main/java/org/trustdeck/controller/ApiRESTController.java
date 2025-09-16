085713bccbc1a38d13c7fbfe73d8b767721e2931
Armin Müller <armin.mueller@bih-charite.de>
2025-09-16 16:30:54 +0200

    Renamed the API REST Controller. Renamed the Object Type REST Controller.

--- src/main/java/org/trustdeck/controller/ApiRESTController.java
+++ src/main/java/org/trustdeck/controller/ApiRESTController.java
@@ -35,7 +35,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping(value = "/api")
-public class ApiController {
+public class ApiRESTController {
 
     /**
      * This method functions as a ping endpoint.
