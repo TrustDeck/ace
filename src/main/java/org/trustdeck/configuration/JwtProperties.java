847a743935d74dcdb10d134715bd440c87bc789a
Armin Müller <armin.mueller@bih-charite.de>
2026-02-11 18:21:06 +0100

    Removed JWT config parameters that are not needed any longer.

--- src/main/java/org/trustdeck/configuration/JwtProperties.java
+++ src/main/java/org/trustdeck/configuration/JwtProperties.java
@@ -1,6 +1,6 @@
 /*
  * Trust Deck Services
- * Copyright 2023-2024 Armin Müller and Eric Wündisch
+ * Copyright 2023-2026 Armin Müller and Eric Wündisch
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.trustdeck.security.authentication.configuration;
+package org.trustdeck.configuration;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,51 +25,30 @@ import org.springframework.context.annotation.Configuration;
 /**
  * The configuration for the JWT properties.
  * 
- * @author Eric Wündisch and Armin Müller
+ * @author Armin Müller and Eric Wündisch
  */
 @Data
 @Configuration
 @ConfigurationProperties
 public class JwtProperties {
 
+    /** The Keycloak server URI. */
+    @Value("${spring.security.oauth2.resourceserver.jwt.server-uri:-}")
+    private String serverUri;
+
+    /** The name of the Keycloak realm where the rights and roles live in. */
+    @Value("${spring.security.oauth2.resourceserver.jwt.realm:-}")
+    private String realm;
+    
     /** The client ID of the keycloak resource. */
     @Value("${spring.security.oauth2.resourceserver.jwt.client-id:trustdeck}")
     private String clientId;
-
-    /** The preferred name for the token, e.g., the username or the ID of the user. */
-    @Value("${spring.security.oauth2.resourceserver.jwt.principal-attribute:preferred_username}")
-    private String principalAttribute;
-
-    /** The claim name to read all group paths. */
-    @Value("${spring.security.oauth2.resourceserver.jwt.role-group-claim-name:groups}")
-    private String roleGroupClaimName;
-
-    /** The bucket/group to store all groups as relationship between domain and role. */
-    @Value("${spring.security.oauth2.resourceserver.jwt.domain-role-group-context-name:Domain}")
-    private String domainRoleGroupContextName;
-
-    /** The bucket/group to store all groups as relationship between project and role. */
-    @Value("${spring.security.oauth2.resourceserver.jwt.project-role-group-context-name:Project}")
-    private String projectRoleGroupContextName;
-
-    /** The user name of an admin account for Keycloak. This is used to handle automatic rights and roles management . */
-    @Value("${spring.security.oauth2.resourceserver.jwt.admin-username:-}")
-    private String adminUsername;
-
-    /** The password for the admin user account. */
-    @Value("${spring.security.oauth2.resourceserver.jwt.admin-password:-}")
-    private String adminPassword;
 
     /** The client secret used for authentication against Keycloak. */
     @Value("${spring.security.oauth2.resourceserver.jwt.client-secret:-}")
     private String clientSecret;
 
-    /** The name of the Keycloak realm where the rights and roles live in. */
-    @Value("${spring.security.oauth2.resourceserver.jwt.realm:-}")
-    private String realm;
-
-    /** The Keycloak server URI. */
-    @Value("${spring.security.oauth2.resourceserver.jwt.server-uri:-}")
-    private String serverUri;
-
+    /** The preferred name for the token, e.g., the username or the ID of the user. */
+    @Value("${spring.security.oauth2.resourceserver.jwt.principal-attribute:preferred_username}")
+    private String principalAttribute;
 }
